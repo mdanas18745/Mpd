5be133ef8bcebfed2f0156dc7a20e874a9c13837
mdanas18745 <facttamizha10384@gmail.com>
2025-04-09 18:37:51 +0530

    Auto-update: 2025-04-09 18:37:51

--- update_mpd.sh
+++ update_mpd.sh
@@ -1,14 +1,10 @@
-cat > ~/update_mpd.sh << 'EOF'
-#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
 
-cd ~/Mpd/MPD
-ffmpeg -i "http://stream3.zecast.com:1935/star-live/sp20239/chunklist_w1096483107.m3u8" \
-  -map 0 -f dash -seg_duration 6 -t 3600 Tamil.mpd
+# Add all changes
+git add .
 
-cd ~/Mpd
-git add MPD/Tamil.mpd
-git commit -m "Auto update Tamil.mpd"
+# Commit with timestamp message
+git commit -m "Auto-update: $(date '+%Y-%m-%d %H:%M:%S')"
+
+# Push to GitHub
 git push origin main
-EOF
-
-chmod +x ~/update_mpd.sh
